db99589578f9bc055cb08dab26016616771083f7
jyz <3124318397@qq.com>
2019-09-17 19:22:51 +0800

    2019-09-17

---
+++ Client1.py
@@ -0,0 +1,47 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: JYZ
+
+import socket
+import os
+import threading
+
+
+class Client():
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = '127.0.0.1'
+        self.port = 9999
+        self.client = self.socket.connect((self.host, self.port))
+        if not os.path.isfile('key.txt'):
+            self.client.send('No id'.encode('utf-8'))
+            _recv = self.client.recv(1024)
+            f = openfile('key.txt', 'w')
+            f.writelines(_recv)
+            f.close()
+            self.client.send(_recv.encode('utf-8'))
+        else:
+            f = openfile('key.txt', 'w')
+            line = f.readlines()
+            self.client.send(str(line).encode('utf-8'))
+
+    def _send(self):
+        while True:
+            target = input(">>>>>>to").strip()
+            line = input(">>>>>>say:").strip()
+            msg = target + ',' + line
+            self.client.send(msg.encode('utf-8'))
+
+    def _recv(self):
+        while True:
+            data = self.client.recv(1024)
+            print(data)
+def main():
+    client = Client()
+    t1 = threading.Thread(target=client._send())
+    t2 = threading.Thread(target=client._recv())
+    t1.start()
+    t2.start()
+
+if __name__ == '__main__':
+    main()
---
+++ Server.py
@@ -0,0 +1,92 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: JYZ
+
+import socket
+import threading
+from queue import Queue
+
+lock = threading.Lock()
+msg_queue = Queue()
+client_dict = dict()
+id = 0
+
+
+class Server():
+    def __init__(self, port):
+        self.port = port
+        self.host = '127.0.0.1'
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serversocket.bind((self.host, self.port))
+        self.serversocket.listen(5)
+        print("ServerSocket Started !")
+        global id
+        global client_dict
+        global msg_queue
+
+    def run(self):
+        while True:
+            clientsocket, address = self.serversocket.accept()
+            try:
+
+                thread = Client(clientsocket, address)
+                thread.start()
+
+            except socket.timeout:
+                print("Client Connection Timeout !")
+
+    def _recv(self):
+        while True:
+            if not msg_queue.empty():
+                msg = self.get_msg(msg_queue)
+                data = '>>>>>来自id:{}的消息:{}'.format(str(msg[0]), str(msg[2]))
+                client_dict[str(msg[1])].send(data.encode('utf8'))
+
+    def get_msg(self, _queue):
+        lock.acquire()
+        _msg = _queue.get()
+        lock.release()
+        return _msg
+
+
+class Client(threading.Thread):
+    def __init__(self, clientsocket, address):
+        super(Client, self).__init__()
+        self.clientsocket = clientsocket
+        self.username = None
+        self.address = address
+        global msg_queue
+        global client_dict
+        global id
+        data = self.clientsocket.recv(1024)
+        if data == 'No id':
+            msg = '你的id是:{}'.format(str(id))
+            id += 1
+            self.clientsocket.send(msg.encode('utf8'))
+            self.username = self.clientsocket.recv(1024)
+            client_dict[self.username] = [self.address, self.clientsocket]
+        else:
+            self.username = str(data)
+
+    def run(self):
+        while True:
+            data = self.clientsocket.recv(1024)
+            msg = self.username + ',' + data
+            self.append(msg)
+
+    def append(self, data):
+        lock.acquire()
+        msg_queue.put(data.split(','))
+        lock.release()
+
+
+def main():
+    server = Server(9999)
+    t1 = threading.Thread(target=server.run())
+    t2 = threading.Thread(target=server._recv())
+    t1.start()
+    t2.start()
+
+
+if __name__ == '__main__':
+    main()
