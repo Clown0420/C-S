0e1a4864861254d8e01afc9dc3bddf60cf795c0e
icanfly <3124318397@qq.com>
2019-10-12 20:20:13 +0800

    file

---
+++ Cluster/new/conf.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python3.6
+# -*- coding: UTF-8 -*-
+# Author: icanfly
+
+
+db_host = ''
+db_port = 3306
+db_name = ''
---
+++ Cluster/new/init.py
@@ -0,0 +1,9 @@
+# !/usr/bin/python3.6
+# -*- coding: UTF-8 -*-
+# Author: icanfly
+
+import socket
+import threading
+
+
+# 接收数据
